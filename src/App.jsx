07054822bb1ec7c910c28698841a3c9438b10cfb
mohamed-janahi <Mohamed.janahi98@gmail.com>
2025-10-26 12:36:45 +0300

    finished the signup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,42 @@
-import './App.css'
+import { useEffect, useState } from "react"
+import { Routes, Route } from "react-router-dom"
+import "./App.css"
+import Home from "./pages/Home"
+import Register from "./components/pages/Register"
 
 const App = () => {
+  const [user, setUser] = useState(null)
 
+  const handleLogOut = () => {
+    setUser(null)
+    localStorage.clear()
+  }
+
+  useEffect(() => {
+    const checkToken = async () => {
+      //If a token exists, sends token to localStorage to persist logged in user
+      const userData = await CheckSession()
+      setUser(userData)
+    }
+    const token = localStorage.getItem("token")
+    // Check if token exists before requesting to validate the token
+    if (token) {
+      checkToken()
+    }
+  }, [])
   return (
-    <h1>Hello</h1>
+    <>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          {/* <Route path="/profile" element={<Profile user={user} />} /> */}
+          {/* <Route path="/signin" element={<SignIn setUser={setUser} />} />
+
+           */}
+        </Routes>
+      </main>
+    </>
   )
 }
 
